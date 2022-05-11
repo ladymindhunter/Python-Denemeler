85bfc1d6add22bb39faa946644d74e194cdb9ee1
Gözde <104469296+ladymindhunter@users.noreply.github.com>
2022-05-11 15:52:23 +0300

    Create Parola_Giriniz.py

---
+++ Parola_Giriniz.py
@@ -0,0 +1,7 @@
+parola= input('Lütfen parola giriniz:')
+if parola==123456:
+    print('teşekkürler!')
+else: parola!=123456
+print('yanlış girdiniz!')
+
+# parolayı doğru girmeme rağmen çalışmıyor, sebebi ne olabilir?
