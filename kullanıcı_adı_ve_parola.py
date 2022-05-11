2a7f2be0fb1d97b4cbe0dd1d66fb2ffa4bb49dbd
Gözde <104469296+ladymindhunter@users.noreply.github.com>
2022-05-11 20:36:36 +0300

    Create kullanıcı_adı_ve_parola.py

---
+++ kullanıcı_adı_ve_parola.py
@@ -0,0 +1,13 @@
+kullanıcı_adı= input('Kullanıcı adı giriniz:' )
+if (kullanıcı_adı== 'ladymindhunter'):
+    print('Kullanıcı adı doğru')
+else:
+    print('Kullanıcı adı hatalı')
+    
+parola=input('Şifre giriniz: ')
+
+if (parola == '123456'):
+    print('Giriş başarılı')
+else:
+    print('Şifre hatalı')
+#kullanıcı adı yanlış girilince yine de parola istiyor.   Nasıl yaparım bilmiyorum.
